47705f3ac73f2898f15432e78248e463277a14fd
Gabriela Burlakova <gabriela.burlakova@gmail.com>
2014-07-29 18:05:40 +0300

    Updates on the functionality

--- Teamwork - RoseChampagne/scripts/game.js
+++ Teamwork - RoseChampagne/scripts/game.js
@@ -3,14 +3,34 @@ var bankerOffers = startGame()[1];
 var player = {hasBox: false,
               boxesOpened: 0,
               box: 0};
+var offerField = document.getElementById('offerField');
+var deal = document.getElementById('deal');
+var noDeal = document.getElementById('no-deal');
+var boxesLenght = 16;
+var offerText;
+var soundPlayer = document.getElementById('player');
+var sound = document.getElementById('sound');
+var soundsLowPrizes = ['sounds/Crowd-Boo-Small.mp3',
+                       'sounds/Failure-WahWah.mp3', 'sounds/Foghorn.mp3',
+                       'sounds/PressYourLuck-Whammy.mp3', 'sounds/Wheel-of-Fortune-Bankrupt.mp3'];
+var soundsHighPrizes = ['sounds/TaDa.mp3',];
+var soundSource = '';
 
-//Add prizes to the aside fields
+//Add sorted prizes by Points to the aside fields
 var prize;
+var prizesSorted = [];
 for (var prizeIndex = 0; prizeIndex < boxes.length; prizeIndex++) {
-    prize = document.getElementById('prize' + (prizeIndex + 1));
-    prize.innerText = boxes[prizeIndex].prize;
-    //TODO Sort the prizes by points TODO
+    prizesSorted.push(boxes[prizeIndex]);
 }
+prizesSorted.sort(function(previousPrize, currentPrize){
+    return previousPrize.points - currentPrize.points;
+});
+
+for (var index = 0; index < prizesSorted.length; index++) {
+    prize = document.getElementById('prize' + (index + 1));
+    prize.innerText = prizesSorted[index].prize;
+}
+
 
 var question;
 question = document.getElementById('question');
@@ -61,6 +81,21 @@ waitingForPlayer.addListener(function(e){
     }
 });
 
+//This function will find index of the boxOpened in order to remove the right side field
+function findIndexByValueAndProp(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+}
+
+
+//This random function helps to get random source for the player
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 for (var boxId = 0; boxId < 16; boxId++) {
     var box = document.getElementById(boxId.toString());
     box.setAttribute('class', 'closedBox');
@@ -92,15 +127,50 @@ for (var boxId = 0; boxId < 16; boxId++) {
                 player.boxesOpened += 1;
                 box.removeAttribute('class');
                 box.setAttribute('class', 'openedBox');
+                //Here we remove the prize from the aside fields and from the with boxes set
+                var propertyToSearch = 'prize';
+                var idPrizeField = findIndexByValueAndProp(prizesSorted, propertyToSearch, boxes[boxId].prize);
+                if (idPrizeField < (prizesSorted.length / 2)) {
+                    soundSource = soundsHighPrizes[getRandomInt(0, soundsHighPrizes.length)];
+                } else {
+                    soundSource = soundsLowPrizes[getRandomInt(0, soundsLowPrizes.length)];
+                }
+                sound.src = soundSource;
+                soundPlayer.load();
+                boxes[boxId].isOpen = true;
                 delete boxes[boxId];
-                //Here we remove the prize from the aside fields
-                var prizeToClose = document.getElementById('prize' + (boxId + 1));
+                boxesLenght -= 1;
+                var prizeToClose = document.getElementById('prize' + (idPrizeField + 1));
                 prizeToClose.innerText = '';
             }
 
             //Check if on the field is left only one box - means end of the game
-            if (boxes.length == 1) {
-                //TODO end of the game TODO
+            if (boxesLenght == 2) {
+                waitingForPlayer(true);
+                question = document.getElementById('question');
+                question.innerText = 'И ко прайм ся';
+                offerField.removeAttribute('class');
+                offerField.setAttribute('class', 'offerFieldVisible');
+                offerField.innerText = 'Would like to swap your box for the one the field?';
+                for (var i = 0; i < boxes.length; i++) {
+                    if (boxes.hasOwnProperty(i + '') && (i != player.box)) {
+                        offerText = boxes[i].prize;
+                    }
+                }
+                deal.setAttribute('class', 'activeDeal');
+                noDeal.setAttribute('class', 'activeNoDeal');
+                deal.removeAttribute('disabled');
+                noDeal.removeAttribute('disabled');
+                deal.addEventListener('click', function(){
+                    offerField.innerText = 'You just swapped ' + boxes[player.box].prize + '\n' + ' for '
+                        + offerText;
+                    waitingForPlayer(true);
+                });
+                noDeal.addEventListener('click', function(){
+                    offerField.innerText = 'You just won ' + boxes[player.box].prize;
+                    waitingForPlayer(true);
+                });
+                return;
             }
 
             //Banker offers if the player has opened 2 boxes
@@ -110,7 +180,7 @@ for (var boxId = 0; boxId < 16; boxId++) {
                 question.innerText = 'И ко прайм ся';
                 player.boxesOpened = 0;
                 //Next lines set class to the offer field in order to show it on the screen
-                var offerField = document.getElementById('offerField');
+                offerField = document.getElementById('offerField');
                 offerField.removeAttribute('class');
                 offerField.setAttribute('class', 'offerFieldVisible');
                 var restPrizesWeight = 0;
@@ -122,7 +192,6 @@ for (var boxId = 0; boxId < 16; boxId++) {
                 restPrizesWeight = restPrizesWeight * 0.70;
                 var closestPoints = 0;
                 var smallestDifference = 200;
-                var offerText;
                 for (var offer in bankerOffers) {
                     var currentDiff = Math.abs(offer - restPrizesWeight);
                     if (currentDiff < smallestDifference) {
@@ -132,19 +201,29 @@ for (var boxId = 0; boxId < 16; boxId++) {
                     }
                 }
                 offerField.innerText = offerText;
-                var deal = document.getElementById('deal');
-                var noDeal = document.getElementById('no-deal');
                 deal.setAttribute('class', 'activeDeal');
                 noDeal.setAttribute('class', 'activeNoDeal');
+                deal.removeAttribute('disabled');
+                noDeal.removeAttribute('disabled');
                 deal.addEventListener('click', function(){
                     offerField.innerText = 'You just swapped ' + boxes[player.box].prize + '\n' + ' for '
                         + offerText;
                     waitingForPlayer(true);
+                    deal.removeAttribute('class');
+                    noDeal.removeAttribute('class');
+                    deal.setAttribute('disabled', 'disabled');
+                    noDeal.setAttribute('disabled', 'disabled');
                 });
                 noDeal.addEventListener('click', function(){
+                    question = document.getElementById('question');
+                    question.innerText = 'Select two boxes';
                     offerField.removeAttribute('class');
                     offerField.setAttribute('class', 'offerFieldHidden');
                     waitingForPlayer(false);
+                    deal.removeAttribute('class');
+                    noDeal.removeAttribute('class');
+                    deal.setAttribute('disabled', 'disabled');
+                    noDeal.setAttribute('disabled', 'disabled');
                 });
             }
         }
